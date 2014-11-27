243115e40ad7f14d83fef5ae333843b7c6a350b9
James <adks3489@gmail.com>
2014-11-27 11:52:50 +0800

    Add Target Urls Add Access-Control-Allow-Origin

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,92 +1,74 @@
-'use strict';
-
-function getAllowHeader () {
-    var allowHeaders = localStorage.getItem('allowHeaders') || '';
-
-    if (!allowHeaders) {
-        allowHeaders = ['Authorization', 'Auth', 'Token', 'Access-Token', 'Access_Token', 'AccessToken', 'Code'];
-    } else {
-        allowHeaders = allowHeaders.split(',');
-    }
-
-    return allowHeaders;
+function getTargetUrls() {
+  var strUrls = localStorage.getItem("targetUrls") || "";
+  return strUrls ? strUrls.split(",") : ["<all_urls>"];
+}
+function getAllowOrign() {
+  var origin = localStorage.getItem("allowOrigin") || "";
+  return origin ? origin : "*";
+}
+function getAllowHeaders() {
+  var headers = localStorage.getItem("allowHeaders") || "";
+  return headers.split(",");
 }
 
-function handleRequest (details) {
-    var header;
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-        header = details.requestHeaders[i];
-        if (header.name === 'Origin' && !header.value) {
-            header.value = details.url || 'http://evil.com';
-            break;
-        }
+function handleRequest(a) {
+  for (var b, c = 0; c < a.requestHeaders.length; ++c){
+    if (b = a.requestHeaders[c], "Origin" === b.name && !b.value) {
+      b.value = a.url || "";
+      break;
     }
-    return {requestHeaders: details.requestHeaders};
+  }
+  return {requestHeaders: a.requestHeaders};
 }
 
-function handleRespone (details) {
-    var header = null,
-        allowHeaders = getAllowHeader(),
-        originFound = false,
-        headerFound = false;
-
-    for (var i = 0, len = details.responseHeaders.length; i < len; ++i) {
-        header = details.responseHeaders[i];
-        if (!originFound || !headerFound) {
-            if (header.name === 'Access-Control-Allow-Origin') {
-                //header.value = origin;
-                originFound = true;
-            }
-            else if (header.name === 'Access-Control-Allow-Headers') {
-                //header.value += ',' + allowHeaders.join(',');
-                headerFound = true;
-            }
-        } else {
-            break;
-        }
-    }
-
-    if (!originFound) {
-        details.responseHeaders.push({
-            name: 'Access-Control-Allow-Origin',
-            value: '*',
-        });
-    }
-    if (!headerFound) {
-        details.responseHeaders.push({
-            name: 'Access-Control-Allow-Headers',
-            value: allowHeaders.join(',')
-        });
-    }
-
-    return {responseHeaders: details.responseHeaders};
+function handleRespone(a) {
+  for (var b = null, c = getAllowHeaders(), d = !1, e = !1, f = 0, g = a.responseHeaders.length; g > f && (b = a.responseHeaders[f], !d || !e); ++f) "Access-Control-Allow-Origin" === b.name ? d = !0 : "Access-Control-Allow-Headers" === b.name && (e = !0);
+  return d || a.responseHeaders.push({
+    name: "Access-Control-Allow-Origin",
+    value: getAllowOrign()
+  }), e || a.responseHeaders.push({
+    name: "Access-Control-Allow-Headers",
+    value: c.join(",")
+  }), { responseHeaders: a.responseHeaders };
 }
 
-function setOn () {
-    chrome.browserAction.setBadgeText({text: 'on'});
-    chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
-    chrome.webRequest.onBeforeSendHeaders.addListener(handleRequest, {urls: ['<all_urls>'], types: ['xmlhttprequest']}, ['blocking', 'requestHeaders']);
-    chrome.webRequest.onHeadersReceived.addListener(handleRespone, {urls: ['<all_urls>'], types: ['xmlhttprequest']}, ['blocking', 'responseHeaders']);
-}
-function setOff () {
-    chrome.browserAction.setBadgeText({text: 'off'});
-    chrome.browserAction.setBadgeBackgroundColor({color: [128, 128, 128, 200]});
-    chrome.webRequest.onBeforeSendHeaders.removeListener(handleRequest);
-    chrome.webRequest.onHeadersReceived.removeListener(handleRespone);
+function setOn() {
+  chrome.browserAction.setBadgeText({
+    text: "On"
+  });
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: [0, 0, 0, 0]
+  });
+
+  var urls = getTargetUrls();
+  chrome.webRequest.onBeforeSendHeaders.addListener(handleRequest, {
+    urls: urls,
+    types: ["xmlhttprequest"]
+  }, ["blocking", "requestHeaders"]);
+  chrome.webRequest.onHeadersReceived.addListener(handleRespone, {
+    urls: urls,
+    types: ["xmlhttprequest"]
+  }, ["blocking", "responseHeaders"]);
 }
 
-if (localStorage.getItem('on')) {
-    setOn();
-} else {
+function setOff() {
+  chrome.browserAction.setBadgeText({
+    text: "Off"
+  });
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: [128, 128, 128, 200]
+  });
+  chrome.webRequest.onBeforeSendHeaders.removeListener(handleRequest);
+  chrome.webRequest.onHeadersReceived.removeListener(handleRespone);
+}
+
+chrome.extension.onRequest.addListener(function(request) {
+  if (request && (request.id == "updateConfig")){
     setOff();
-}
+    setOn();
+  }        
+});
 
-chrome.browserAction.onClicked.addListener(function () {
-    if (localStorage.getItem('on')) {
-        localStorage.setItem('on', '');
-        setOff();
-    } else {
-        localStorage.setItem('on', '1');
-        setOn();
-    }
+localStorage.getItem("on") ? setOn() : setOff(), chrome.browserAction.onClicked.addListener(function() {
+  localStorage.getItem("on") ? (localStorage.setItem("on", ""), setOff()) : (localStorage.setItem("on", "1"), setOn())
 });
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,36 +1,39 @@
-(function () {
-    'use strict';
-
-    var saveBtn = document.querySelector('.options-save');
-    var resumeBtn = document.querySelector('.options-resume');
-    var tips = document.querySelector('.options-tips');
-    var headersInput = document.getElementById('allow-headers');
-
-    headersInput.value = localStorage.getItem('allowHeaders') || '';
-
-    saveBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        saveBtn.disabled = true;
-
-        tips.innerHTML = '';
-        localStorage.setItem('allowHeaders', headersInput.value);
-
-        setTimeout(function () {
-            saveBtn.disabled = false;
-            tips.innerHTML = 'Saved';
-        }, 100);
+! function() {
+    var a = document.querySelector(".options-save");
+    var b = document.querySelector(".options-resume");
+    var c = document.querySelector(".options-tips");
+    var urls = document.getElementById("targetUrls");
+    var origin = document.getElementById("allowOrigin");
+    var headers = document.getElementById("allowHeaders");
+    urls.value = localStorage.getItem("targetUrls") || "";
+    origin.value = localStorage.getItem("allowOrigin");
+    headers.value = localStorage.getItem("allowHeaders") || "";
+    a.addEventListener("click", function(b) {
+      b.preventDefault();
+      a.disabled = !0;
+      c.innerHTML = "";
+      localStorage.setItem("targetUrls", urls.value);
+      localStorage.setItem("allowOrigin", origin.value);
+      localStorage.setItem("allowHeaders", headers.value);
+      setTimeout(function() {
+        a.disabled = !1;
+        c.innerHTML = "Saved";
+        chrome.extension.sendRequest({id:"updateConfig"});
+      }, 100);
     });
-
-    resumeBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        resumeBtn.disabled = true;
-        tips.innerHTML = '';
-        headersInput.value = headersInput.placeholder;
-        localStorage.setItem('allowHeaders', headersInput.value);
-        setTimeout(function () {
-            resumeBtn.disabled = false;
-        }, 100);
-    });
-})();
+    b.addEventListener("click", function(a) {
+      a.preventDefault();
+      b.disabled = !0;
+      c.innerHTML = "";
+      urls.value = urls.placeholder;
+      origin.value = origin.placeholder;
+      headers.value = headers.placeholder;
+      localStorage.setItem("targetUrls", urls.value);
+      localStorage.setItem("allowOrigin", origin.value);
+      localStorage.setItem("allowHeaders", headers.value);
+      setTimeout(function() {
+        b.disabled = !1;
+        chrome.extension.sendRequest({id:"updateConfig"});
+      }, 100)
+    })
+}();
